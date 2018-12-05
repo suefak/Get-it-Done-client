92f588d5417babec352db6ef21d0e89bbea4da12
suefak <suefakunle@gmail.com>
2018-12-05 17:12:36 -0500

    fixed heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://hidden-thicket-26795.herokuapp.com/s',
+  production: 'https://hidden-thicket-26795.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
